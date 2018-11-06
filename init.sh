a075cbabb623bffe9913ca4d6fae6d3b5a54fc3e
Xmader <xmader@outlook.com>
2018-11-06 20:55:53 +0800

    安装依赖

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 
-cordova create AriaNgGUIAndroid com.xmader.aria_ng_gui_android AriaNgGUI
+# cordova create AriaNgGUIAndroid com.xmader.aria_ng_gui_android AriaNgGUI
 
-cordova platform add android
+# cordova platform add android
 
-cordova plugin add https://github.com/petervojtek/cordova-plugin-shell-exec.git
+# cordova plugin add https://github.com/petervojtek/cordova-plugin-shell-exec.git
+
+cordova prepare android
 
 # 将'<script src="cordova.js"></script><script src="main.js"></script>'放入index.html中
 
