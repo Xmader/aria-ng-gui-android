f648ecdd0a328031c58dcdd2b8aa2c43c57cff9e
Xmader <xmader@outlook.com>
2018-11-06 21:30:24 +0800

    v1.0.0 - AriaNgGUI的Android手机版正式发布

--- www/main.js
+++ www/main.js
@@ -1,9 +1,38 @@
 
 document.addEventListener('deviceready', function () {
+    const aria2FileURL = cordova.file.applicationDirectory + "www/aria2/android/aria2c"
+    const aria2ConfFileURL = cordova.file.applicationDirectory + "www/aria2/aria2.conf"
 
-    window.ShellExec.exec('uptime', function (res) {
-        console.log('exit status: ' + res.exitStatus)
-        console.log('cmd output: ' + res.output)
-    })
+    const successCallback = function (copiedFileEntry) {
+        const copiedAria2FileURL = copiedFileEntry.nativeURL.replace("file://", "")
+        const copiedAria2ConfFileURL = copiedAria2FileURL.replace(/\/aria2c$/, "/aria2.conf")
+        // console.log(copiedAria2FileURL)
+        // console.log(copiedAria2ConfFileURL)
+
+        window.ShellExec.exec(["chmod", "0777", copiedAria2FileURL], function (res) {
+            console.log('exit status: ' + res.exitStatus)
+            console.log('cmd output: ' + res.output)
+        })
+        window.ShellExec.exec([copiedAria2FileURL, "--conf-path=" + copiedAria2ConfFileURL], function (res) {
+            console.log('exit status: ' + res.exitStatus)
+            console.log('cmd output: ' + res.output)
+        })
+
+    }
+    const errorCallback = function (e) { console.error(e) }
+
+    // 复制aria2.conf
+    window.resolveLocalFileSystemURL(aria2ConfFileURL, function (fileEntry) {
+        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (dirEntry) {
+            fileEntry.copyTo(dirEntry, fileEntry.name, function () { }, errorCallback);
+        })
+    }, errorCallback)
+
+    // 复制aria2c
+    window.resolveLocalFileSystemURL(aria2FileURL, function (fileEntry) {
+        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (dirEntry) {
+            fileEntry.copyTo(dirEntry, fileEntry.name, successCallback, errorCallback);
+        })
+    }, errorCallback)
 
 }, false);
