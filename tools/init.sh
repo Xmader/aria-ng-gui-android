3a992d3155ce625ce6e8583a7ee874e8a44a07c2
Xmader <xmader@outlook.com>
2019-10-16 17:42:44 -0400

    upgrade dependencies

--- tools/init.sh
+++ tools/init.sh
@@ -6,10 +6,10 @@ cordova plugin add cordova-plugin-file
 cordova plugin add cordova-plugin-app-version
 cordova plugin add cordova-plugin-android-permissions
 
-cordova platform add android@~6.3.0
+cordova platform add android@8
 
 cordova prepare android
 
 # 将'<script src="cordova.js"></script><script src="js/babel-polyfill.min.js"></script><script src="main.js"></script>'放入index.html中
 
-cordova build android --release --archs=arm --device --ant
+cordova build android --release --archs=arm --device
