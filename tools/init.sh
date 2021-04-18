8a6ecb189126eac6dfdc14ca16f40c02ba11f285
Xmader <xmader@outlook.com>
2021-04-18 14:37:05 -0400

    upgrade cordova-android

--- tools/init.sh
+++ tools/init.sh
@@ -6,8 +6,7 @@ cordova plugin add cordova-plugin-file
 cordova plugin add cordova-plugin-app-version
 cordova plugin add cordova-plugin-android-permissions
 
-cordova platform add android@8
-
+cordova platform add android@9
 cordova prepare android
 
 # 将'<script src="cordova.js"></script><script src="js/babel-polyfill.min.js"></script><script src="main.js"></script>'放入index.html中
